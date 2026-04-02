a7c938ec25cb57adf1b155c88621544146632e7e
Youssef Briki <youssef.briki@umontreal.ca>
2026-04-02 19:16:07 -0400

    Update

--- src/data-processing/california/scripts/download_ceed_waveforms.py
+++ src/data-processing/california/scripts/download_ceed_waveforms.py
@@ -4,6 +4,7 @@ import os
 
 save_dir = "/scratch/brikiyou/ift3710/data/ceed_waveforms"
 os.makedirs(save_dir, exist_ok=True)
+'''
 save_path = os.path.join(save_dir, "events_train.csv")
 
 print("Downloading from Hugging Face...")
@@ -11,12 +12,12 @@ df = pd.read_csv("hf://datasets/AI4EPS/CEED/events_train.csv")
 df.to_csv(save_path, index=False)
 
 print(f"Success! Saved permanently to: {save_path}")
-
+'''
 
 ceed_test = load_dataset(
     "AI4EPS/CEED", 
-    name="station_test", 
-    split="test", 
+    name="station_train", 
+    split="train", 
     trust_remote_code=True,
     cache_dir="/scratch/brikiyou/ift3710/data/ceed_waveforms"
 )
